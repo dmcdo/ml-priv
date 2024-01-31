42d14de0844fa93db14877f3af688ead353b0907
dylan <dylanalexandermcdougall@outlook.com>
2024-01-31 03:56:46 -0500

    ads

--- tiktaktoe/Experiment.java
+++ tiktaktoe/Experiment.java
@@ -3,42 +3,90 @@ import java.util.ArrayList;
 public class Experiment {
     public char[][] board;
     public char startingPlayer;
-    public ArrayList<int[]> hist;
+    public ArrayList<int[]> moveHist;
 
     public Experiment(char startingPlayer) {
         assert startingPlayer == 'X' || startingPlayer == 'O';
 
-        this.hist = new ArrayList<>();
+        this.moveHist = new ArrayList<>();
         this.startingPlayer = startingPlayer;
         this.board = new char[][] {
-            {'\0', '\0', '\0'},
-            {'\0', '\0', '\0'},
-            {'\0', '\0', '\0'}
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
         };
     }
 
     public void move(int i, int j) {
+        assert board[i][j] == ' ';
+        assert !gameIsOver();
+
         board[i][j] = nextPlayer();
-        hist.add(new int[] {i, j});
+        moveHist.add(new int[] {i, j});
     }
 
     public char nextPlayer() {
-        if (hist.size() % 2 == 0)
+        if (moveHist.size() % 2 == 0)
             return startingPlayer == 'X' ? 'X' : 'O';
         else
             return startingPlayer == 'X' ? 'O' : 'X';
     }
 
+    // public static int[][] getEmptyCells(char[][] board) {
+    //     ArrayList<int[]> empty = new ArrayList<>();
 
+    //     for (int i = 0; i < 3; i++)
+    //         for (int j = 0; j < 3; j++)
+    //             if (board[i][j] == ' ')
+    //                 empty.add(new int[] {i, j});
 
-    public static int[][] getEmptyCells(char[][] board) {
-        ArrayList<int[]> empty = new ArrayList<>();
+    //     return (int[][])empty.toArray();
+    // }
 
+    public boolean gameIsOver() {
+        final int[][][] lines = {
+            {{0, 0}, {1, 0}, {2, 0}},
+            {{0, 1}, {1, 1}, {2, 1}},
+            {{0, 2}, {1, 2}, {2, 2}},
+            {{0, 0}, {0, 1}, {0, 2}},
+            {{1, 0}, {1, 1}, {1, 2}},
+            {{2, 0}, {2, 1}, {2, 2}},
+            {{0, 0}, {1, 1}, {2, 2}},
+            {{2, 0}, {1, 1}, {0, 2}}
+        };
+
+        // Check if a player has won.
+        for (int[][] line : lines)
+            if (board[line[0][0]][line[0][1]] == board[line[1][0]][line[1][1]] &&
+                board[line[1][0]][line[1][1]] == board[line[2][0]][line[2][1]] &&
+                board[line[2][0]][line[2][1]] != ' ')
+                return true;
+
+        // Check if there are any empty spaces.
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
-                if (board[i][j] == '\0')
-                    empty.add(new int[] {i, j});
+                if (board[i][j] == ' ')
+                    return false;
 
-        return (int[][])empty.toArray();
+        // The game has ended in a tie.
+        return true;
+    }
+
+    public String getBoardString() {
+        return String.format(
+            " |0|1|2\n" +
+            "0|%c|%c|%c\n" +
+            "1|%c|%c|%c\n" +
+            "2|%c|%c|%c\n",
+            board[0][0],
+            board[1][0],
+            board[2][0],
+            board[0][1],
+            board[1][1],
+            board[2][1],
+            board[0][2],
+            board[1][2],
+            board[2][2]
+        );
     }
 }
--- tiktaktoe/PerformanceSys.java
+++ tiktaktoe/PerformanceSys.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class PerformanceSys {
-    // public static 
+    public static Experiment trainHumanTeacher(Hypothesis hyp, char startingPlayer) {
+        Scanner sc = new Scanner(System.in);
+        Experiment exp = new Experiment(startingPlayer);
+
+        System.out.println("You will be O.");
+        System.out.println("Player " + exp.nextPlayer() + " will go first.");
+
+        while (!exp.gameIsOver()) {
+            if (exp.nextPlayer() == 'X' && false) {
+                // TODO
+            }
+            else {
+                System.out.println(exp.getBoardString());
+                System.out.println("--------");
+                System.out.println("Enter your move in the form of 'x y':");
+                int i = sc.nextInt();
+                int j = sc.nextInt();
+                exp.move(i, j);
+            }
+        }
+
+        sc.close();
+        return null; // TODO
+    }
 }
