487975248f620934d3bfe1785a187a099620aa49
dylan <dylanalexandermcdougall@outlook.com>
2024-02-20 00:04:39 -0500

    asddas

---
+++ decision-tree-learning/example.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Example:
+    def __init__(self, vals, result):
+        self.vals = vals
+        self.result = result
+
+
+def entropy(examples: List[Example]):
+    
---
+++ decision-tree-learning/main.py
@@ -0,0 +1,12 @@
+from attributes import Attributes
+from example import Example
+from typing import List
+
+
+def ID3(examples: List[Example], attributes: Attributes):
+    
+
+
+
+if __name__ == "__main__":
+    attrs = Attributes("tennis-attr.txt")
